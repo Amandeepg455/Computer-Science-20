59bed8ffc1703601ae26b10b8078eac27908a5b9
25063002 <25063002@L815WD220014.edu.cbe.ab.ca>
2022-09-12 13:18:49 -0600

    1:18

--- Chapter 2/src/TicTacToe.java
+++ Chapter 2/src/TicTacToe.java
@@ -3,6 +3,18 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 	
+		 System.out.println("         |         |");
+	        System.out.println("         |         |");
+	        System.out.println("         |         |");
+	        System.out.println("-----------------------------");
+	        System.out.println("         |         |");
+	        System.out.println("         |    X    |");
+	        System.out.println("         |         |");
+	        System.out.println("-----------------------------");
+	        System.out.println("         |         |");
+	        System.out.println("         |         |");
+	        System.out.println("         |         |");
+
 
 	}
 
