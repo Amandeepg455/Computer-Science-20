9eceeb67a11059493ef5db8ed74e588a9bfa78f7
25063002 <25063002@L815WD220014.edu.cbe.ab.ca>
2022-09-14 13:35:39 -0600

    1:35 PM

--- Chapter 2/src/crt.java
+++ Chapter 2/src/crt.java
@@ -7,13 +7,33 @@ public class crt {
 	
 		/*
 		 
-3. TestAverage.java
- Calculates test averages
- Amandeep
- September 13,2022
- 
- */
+					3. TestAverage.java
+				 Calculates test averages
+				 Amandeep 
+				 September 13,2022
+				 
 		
+			4. the AverageScore displays the average of a set of scores 
+			
+			
+			 5. package gradeCalculator
+			
+			6. public class AverageScore
+			
+				 7. Source code is written by a human or programmer. Byte code is not written by humans or programmers. 
+				  12. print method prints the string but does not move the cursor to a new line while the println method prints the string and moves the cursor to a new line. 
+*/ 
+   
+
+ 
+		
+
+	
+	
+		
+	
+		
+
 		
  
 	
