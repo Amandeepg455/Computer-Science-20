ba2ce2c744aafea00f08ac777db60c1b54056ee8
25063002 <25063002@L815WD220014.edu.cbe.ab.ca>
2022-09-14 13:30:36 -0600

    1:29 Pm

---
+++ Chapter 2/src/crt.java
@@ -0,0 +1,23 @@
+
+public class crt {
+
+	public static void main(String[] args) {
+		
+
+	
+		/*
+		 
+3. TestAverage.java
+ Calculates test averages
+ Amandeep
+ September 13,2022
+ 
+ */
+		
+		
+ 
+	
+	}
+
+}
+
